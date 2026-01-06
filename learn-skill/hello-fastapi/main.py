5e4c412b64376094dfe1fed201a0aff9f81a95be
SafdarAliShah <programmersafdar@live.com>
2026-01-06 21:22:26 +0500

    feature: routes

--- learn-skill/hello-fastapi/main.py
+++ learn-skill/hello-fastapi/main.py
@@ -14,6 +14,26 @@ def root() -> dict[str, str]:
     """Root endpoint returning a hello world message."""
     return {"message": "Hello, World!"}
 
+@app.get("/t/todos")
+def todo() -> list[dict[str,int|str]]:
+    return [{"id":1,"task":"buy the book"},
+            {"id":2,"task":"Now read the book"}]
+@app.get("/t/todos/{task_id}")
+async def todo_by_id(task_id:int,include_details: bool = False):
+    if task_id<0:
+        return{"error":"Task_Id must be greater than 0"}
+    if include_details:
+        return[
+            {"id":task_id,
+             "todos":"Do Something meaningful",
+             "details":"Like Prgramming"}
+        ]
+    return [
+        {
+            "id":task_id,
+             "todos":"Do Something meaningful",
+        }
+    ]
 
 @app.get("/health")
 def health() -> dict[str, str]:
