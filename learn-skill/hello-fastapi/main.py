d0b02ac75e5d25ca6fd1c57c7e431c0e89707edd
SafdarAliShah <programmersafdar@live.com>
2026-01-07 20:48:35 +0500

    updated

--- learn-skill/hello-fastapi/main.py
+++ learn-skill/hello-fastapi/main.py
@@ -1,14 +1,23 @@
 """FastAPI Hello World Application."""
-
+import time
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+class TodoItem(BaseModel):
+    id:int
+    task:str = "Default Value of Task"
+    timeofcreate: str
+
+class TodoItemResponse(BaseModel):
+    id:int
+    task:str
+    timeofcreate: str
+    is_completed: bool = 'false'
 app = FastAPI(
     title="Hello FastAPI",
     description="A simple hello world FastAPI application",
     version="0.1.0",
 )
-
-
 @app.get("/")
 def root() -> dict[str, str]:
     """Root endpoint returning a hello world message."""
@@ -35,7 +44,14 @@ async def todo_by_id(task_id:int,include_details: bool = False):
         }
     ]
 
+@app.post("/t/todos")
+def add_todo(todo: TodoItem) -> TodoItemResponse:
+    return todo;
 @app.get("/health")
 def health() -> dict[str, str]:
     """Health check endpoint."""
     return {"status": "healthy"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0",port=9999)
